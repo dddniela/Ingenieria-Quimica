95b0f46040f6e2395bd2157026488b9c01ac7079
MarckGabriel2 <73321649+MarckGabriel2@users.noreply.github.com>
2023-06-16 00:56:30 -0600

    cambios

--- src/View/comunidades-vista.php
+++ src/View/comunidades-vista.php
@@ -1,6 +1,6 @@
 <div class="row g-0">
     <div class="position-relative w-100 overflow-hidden">
-        <img class="w-100 img-fluid" src="img/IBQ-IQ/P41.png" alt="" />
+        <img class="w-100 img-fluid" src="img/IBQ-IQ/capituladoIQ.webp" alt="" />
         <div class="position-absolute top-50 start-50 translate-middle w-100">
             <div class="d-flex flex-column justify-content-center align-items-center text-center">
                 <h1 class="fw-bold text-warning">Comunidades Estudiantiles</h1>
