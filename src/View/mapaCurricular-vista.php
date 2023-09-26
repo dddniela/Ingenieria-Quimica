4e7f19190c2b9df73ed7cb1e8035f2d8996f217b
MarckGabriel2 <73321649+MarckGabriel2@users.noreply.github.com>
2023-09-26 12:36:45 -0600

    corrección del responsive de las formas de titulación

--- src/View/mapaCurricular-vista.php
+++ src/View/mapaCurricular-vista.php
@@ -692,45 +692,38 @@
             </div>
 
             <div class="col-lg-6 col-12 p-4">
-                <div class="d-flex justify-content-center align-items-center w-100 h-100">
-                    <div class="row g-0">
-                        <div class="col-12">
-                            <h2 class="sectionTitle text-center font-bold m-3">Requisitos para tramitar la titulación:
-                            </h2>
+                    <div class="d-flex justify-content-center align-items-center w-100 h-100">
+                        <div class="row g-0 col-12">
+                            <h2 class="sectionTitle text-center font-bold m-3">Requisitos para tramitar la titulación:</h2>
                             <div class="sectionSeparator"></div>
-                        </div>
-                        <div class="col-12" style="text-align: center;">
-                            <p class="text-dark" style="text-align: justify;">
-                                El egresado debe cumplir con:
+                            <div class="col-12" style="text-align: center;">
+                                <p style="text-align: justify;">El egresado debe cumplir con:</p>
+                                <ul class="my-3 font-bold" style="text-align: justify">
+                                    <li>La acreditación del 100% de los créditos de su plan de estudios.</li>
+                                    <li>La acreditación de un programa de lengua extranjera presentando un certificado o
+                                        constancia.*</li>
+                                    <li>Constancia de no inconveniencia para la realización del acto protocolario
+                                        de titulación integral emitida por el Departamento de Servicios Escolares o
+                                        su equivalente en los Institutos Tecnológicos Descentralizados.</li>
+                                    <li>Documento probatorio de no adeudo económico, material o equipo con
+                                        las oficinas, laboratorios y centro de información en la institución de la cual
+                                        egresó.</li>
+                                    <li>Liberación del proyecto de titulación integral emitida por él (la) Jefe(a) de
+                                        Departamento Académico.</li>
+                                    <li>Presentar acto protocolario del proyecto de titulación integral.</li>
+                                </ul>
+                            </div>
+                            <p class="text-dark text-break" style="text-align: left;">
+                                *Puede variar según el plan de estudios.<br><br>
+                                Solicita información en:<br>
+                                coor_titulacion@veracruz.tecnm.mx <br>
+                                titulacion_quimica@veracruz.tecnm.mx.
                             </p>
-                        </div>
-                        <div class="bg-light">
-                            <ul class="my-3 font-bold" style="text-align: justify">
-                                <li>La acreditación del 100% de los créditos de su plan de estudios.</li>
-                                <li>La acreditación de un programa de lengua extranjera presentando un certificado o
-                                    constancia.*</li>
-                                <li>Constancia de no inconveniencia para la realización del acto protocolario
-                                    de titulación integral emitida por el Departamento de Servicios Escolares o
-                                    su equivalente en los Institutos Tecnológicos Descentralizados.</li>
-                                <li>Documento probatorio de no adeudo económico, material o equipo con
-                                    las oficinas, laboratorios y centro de información en la institución de la cual
-                                    egresó.</li>
-                                <li>Liberación del proyecto de titulación integral emitida por él (la) Jefe(a) de
-                                    Departamento Académico.</li>
-                                <li>Presentar acto protocolario del proyecto de titulación integral.</li>
-                            </ul>
-                        </div>
-                        <p class="text-dark" style="text-align: justify;">
-                        *Puede variar según el plan de estudios. 
-                        </p>
-                        <p class="text-dark" style="text-align: left;">
-                        Solicita información en: coor_titulacion@veracruz.tecnm.mx/titulacion_quimica@veracruz.tecnm.mx.
-                        </p>
-                        <div class="justify-content-center text-center">
+                            <div class="justify-content-center text-center">
                                 <p><a class="btn-warning w-auto btn font-bold" target="_blank" href="https://tinyurl.com/3kpuhew9">Formas de titulación</a></p>
+                            </div>
                         </div>
                     </div>
-                </div>
             </div>
 
 
